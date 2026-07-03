e1d0fa02e0f65beb5577ff51e80bb2cccb1767a3
Galindo-Barboza, Alberto Jorge <aljogaba@gmail.com>
2026-07-03 14:52:43 -0600

    Update phylogeny.js

--- assets/js/phylogeny.js
+++ assets/js/phylogeny.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const API_ENDPOINT = "https://supplemental-decor-announcements-quest.trycloudflare.com";
+const API_ENDPOINT = "https://marion-villa-sent-borough.trycloudflare.com/analyze";
 
 const form = document.getElementById("sequenceAnalysisForm");
 const sequenceInput = document.getElementById("sequenceInput");
